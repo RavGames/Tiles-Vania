d09ebe28d7ffcdad77fdf71636af9a83d9cf6baf
RavGames <RaiderRickG@gmail.com>
2021-09-29 23:39:05 +0530

    Imported the CrossPlatformInput from Standard Assets in unity and Player Movement
    we imported the crossplatformInput from the Standard Assets and used the rigidbody 2d to move the player.

---
+++ Tiles Vania/Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float runSpeed = 5f;
+    private Rigidbody2D rigidbody2d;
+
+    private void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Run();
+    }
+
+    private void Run()
+    {
+        float x = CrossPlatformInputManager.GetAxis("Horizontal") * runSpeed;// this value is between -1 and +1
+        Vector2 playerVelocity = new Vector2(x, rigidbody2d.velocity.y);
+        rigidbody2d.velocity = playerVelocity;
+
+    }
+
+
+
+
+
+
+}//CLASS
